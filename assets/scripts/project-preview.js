108e273a663de5a42943dd46562d3eac06b23f06
tuesday327 <217101731+amarahwork@users.noreply.github.com>
2025-06-28 20:38:43 -0500

    redo home, add outtakes

--- assets/scripts/project-preview.js
+++ assets/scripts/project-preview.js
@@ -1,11 +1,11 @@
 //Homepage Project Previews
 $(function(){
-    $(".row").mouseover(function() {
+    $(".project").mouseover(function() {
         let project = $(this).attr("id");
         $("body").css("background-image", ("url(assets/layout/preview/" + project + ".gif)"));
     });
 
-    $(".projects").mouseout(function() {
+    $(".project").mouseout(function() {
         $("body").css("background-image", "none");
     });
 });     
