75752b5ac897c4a46304d9a7fb094c2592440b65
tuesday327 <217101731+amarahwork@users.noreply.github.com>
2025-09-03 00:14:44 -0500

    change design entirely
    and got rid of misplaced files

--- assets/scripts/project-preview.js
+++ assets/scripts/project-preview.js
@@ -2,10 +2,7 @@
 $(function(){
     $(".project").mouseover(function() {
         let project = $(this).attr("id");
-        $("main").css("background-image", ("url(assets/layout/preview/" + project + ".png)"));
-    });
-
-    $(".project").mouseout(function() {
-        $("main").css("background-image", "none");
+        $("main").css("background-image", ("url(assets/layout/preview/" + project + "_dark.png)"));
+        $(".project a").css("background-image", ("url(assets/layout/preview/" + project + "_light.png)"));
     });
 });     
