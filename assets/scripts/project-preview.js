18835e552b9ff307bffe57aadf1e8900e57bfaf3
tuesday327 <217101731+amarahwork@users.noreply.github.com>
2026-01-06 00:02:12 -0600

    big update

--- assets/scripts/project-preview.js
+++ assets/scripts/project-preview.js
@@ -4,8 +4,4 @@ $(function(){
         let project = $(this).attr("id");
         $("main").css("background-image", ("url(assets/layout/preview/" + project + "_dark.png)"));
     });
-    
-    $(".project").mouseout(function() {
-        $("main").css("background-image", "none");
-    });
 });     
