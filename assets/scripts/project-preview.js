2bd27a0dd5da523c8730a3b71e08b5191b46da90
tuesday327 <217101731+amarahwork@users.noreply.github.com>
2026-01-28 12:54:29 -0600

    site move

--- assets/scripts/project-preview.js
+++
@@ -1,7 +0,0 @@
-//Homepage Project Previews
-$(function(){
-    $(".project").mouseover(function() {
-        let project = $(this).attr("id");
-        $("main").css("background-image", ("url(assets/layout/preview/" + project + "_dark.png)"));
-    });
-});     
